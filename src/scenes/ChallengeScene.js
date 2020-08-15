48000f7b8ea8b2fb2bb6f3e6539c534d2f2e74dd
Carlos Anriquez <canriquez@gmail.com>
2020-08-15 08:43:03 -0300

    competes challenge end evaluation

--- src/scenes/ChallengeScene.js
+++ src/scenes/ChallengeScene.js
@@ -18,7 +18,8 @@ export class ChallengeScene extends Phaser.Scene {
             htextures: ['hero01'],
             dtextures: ['hero01', 'hero01', 'hero01'],
             positions: [290, 365, 450],
-            winFactor: 2, //Affects deamons damage on hero 0-1. 0 easy
+            winFactor: 0.1, //Affects deamons damage on hero 0-1. 0 easy
+            extraPower: 20, //increses player power for the match
             hnames: ['dev Warrior'],
             dnames: ['Deamon-1', 'Deamon-2', 'Deamon-3'],
             dPBars: [[260, 20], [380, 20], [380, 60],],
@@ -26,7 +27,13 @@ export class ChallengeScene extends Phaser.Scene {
                 skills: 50,
                 motivation: 50,
                 courage: 50,
-                deamonx: 90
+                deamonx: 90,
+            },
+            penalty: {
+                skills: -5,
+                motivation: -10,
+                courage: -20,
+                fear: 20,
             }
         }
     }
@@ -230,7 +237,7 @@ export class ChallengeScene extends Phaser.Scene {
         console.log('HEHEH: Hero ATTACKS Demon :' + data);
         let sDeamon = this.enemies[data.id].globals.corazon;
         let hero = this.heroes[0].globals.corazon;
-        let power = Math.ceil(hero.hitPower()[data.weapon]) * 1; //erase this 20 -  only for test
+        let power = Math.ceil(hero.hitPower()[data.weapon]) * this.challengeData.extraPower;
 
         console.log('data coming from ui | hero hit is: ');
         console.log(data.weapon)
@@ -306,6 +313,11 @@ export class ChallengeScene extends Phaser.Scene {
 
     endChallenge() {
         console.log('Challenge ending...');
+        if (this.heroes[0].alive) {
+            console.log("challenge success");
+        } else {
+            console.log("challenge lost")
+        }
         //message on challsnge end - Game over or challenge success
         //go back to map scene
         //put to sleep this scene.
